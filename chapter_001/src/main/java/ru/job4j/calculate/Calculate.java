0466a18e2634cca9b359b38b2503b8929dac1ae9
Maxim Azarkov <aza-maxim@yandex.ru>
2019-02-17 22:55:37 +0700

    1.3. JavaDoc. Code convention[#113286]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Класс для вывода текста приветсивия
+ * @author maximazarkov
+ * @since 17.02.2019
+ * @version 0.1
+ */
+
 public class Calculate {
+    
+    /**
+     * main. Конструктор. Выводит строку "hello Job4j" в консоль
+     * @param args - args
+     */
     public static void main (String[] args){
         System.out.println("hello Job4j");
     }

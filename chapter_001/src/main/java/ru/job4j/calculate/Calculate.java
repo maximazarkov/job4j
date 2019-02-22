8858b1112d4ac849cc45b2674d0adb8cae0aa651
Maxim Azarkov <aza-maxim@yandex.ru>
2019-02-23 00:33:53 +0700

    3.1. Элементарный калькулятор.[#113277]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,28 +1,71 @@
 package ru.job4j.calculate;
 
 /**
- * Calculate
- * @author Maxim Azarkov (aza-maxim@yandex.ru)
- * @version $Id$
- * @since 0.1 (17.02.2019)
+ * Calculate.
+ * @author Maxim Azarkov (aza-maxim@yandex.ru).
+ * @version $Id$.
+ * @since 0.2 (22.02.2019).
  */
 
 public class Calculate {
-    
     /**
-     * main. Конструктор. Выводит строку "hello Job4j" в консоль
-     * @param args - args
-	 * @since 0.1 (17.02.2019)
+     * @since 0.2 (22.02.2019).
      */
-    public static void main(String[] args) {
-        System.out.println("hello Job4j");
+    private double result;
+
+    /**
+     * Method add - sum two number  (first+second).
+     * @param first - first number (double).
+     * @param second - second number (double).
+     * @since 0.2 (22.02.2019).
+     */
+    public void add(double first, double second) {
+        this.result = first + second;
     }
 
-	/**
-     * Method echo
-     * @param name - your name
-	 * @return Echo plus your name.
-	 * @since 0.2 (20.02.2019)
+    /**
+     * Method subtract - sub two number (first-second).
+     * @param first - first number (double).
+     * @param second - second number (double).
+     * @since 0.2 (22.02.2019).
+     */
+    public void subtruct(double first, double second) {
+        this.result = first - second;
+    }
+
+    /**
+     * Method div - div two number (first/second).
+     * @param first - first number (double).
+     * @param second - second number (double).
+     * @since 0.2 (22.02.2019).
+     */
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    /**
+     * Method multiple - sum two number (first*second).
+     * @param first - first number (double).
+     * @param second - second number (double).
+     * @since 0.2 (22.02.2019).
+     */
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    /**
+     * Method getResult - return result (double).
+     * @since 0.2 (22.02.2019).
+     */
+    public double getResult() {
+        return this.result;
+    }
+
+    /**
+     * Method echo.
+     * @param name - your name.
+     * @return Echo plus your name.
+     * @since 0.2 (20.02.2019).
      */
     public String echo(String name) {
         return "Echo, echo, echo : " + name;
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -4,23 +4,20 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 /**
- * Test
- * @author Maxim Azarkov (aza-maxim@yandex.ru)
- * @version $Id$
- * @since 0.2 (20.02.2019)
-  */
+ * Test.
+ * @author Maxim Azarkov (aza-maxim@yandex.ru).
+ * @version $Id$.
+ * @since 0.2 (22.02.2019).
+ */
 
 public class CalculateTest {
-     /**
-     * Test echo(name).
-     */
+	/**
+	 * Test echo(name).
+	 */
 	/*public void when...Then... () {}*/
 	@Test
-    public void whenTakeNameThenThreeEchoPlusName() {
+	public void whenTakeNameThenThreeEchoPlusName() {
 		//Определяем ввод данных
 		String input = "Azarkov Maxim";
 		// определяем ожидаемый результат (или поведение)
@@ -30,13 +27,13 @@ public class CalculateTest {
 		String result = calc.echo(input);
 		//завершающий этап. Сравнение реального и ожидаемого резальтатов программы
 		assertThat(result, is(expect));
-    }
+	}
 
-     /**
-     * Test echo(null) NPE.
-     */
+	/**
+	 * Test echo(null) NPE.
+	 */
 	@Test
-    public void whenTakeNullThenThreeEchoPlusNull() {
+	public void whenTakeNullThenThreeEchoPlusNull() {
 		//Определяем ввод данных
 		String input = "Null";
 		// определяем ожидаемый результат (или поведение)
@@ -46,26 +43,79 @@ public class CalculateTest {
 		String result = calc.echo(input);
 		//завершающий этап. Сравнение реального и ожидаемого резальтатов программы
 		assertThat(result, is(expect));
+	}
+
+	/**
+	 * Test add.
+	 */
+	@Test
+	public void whenAddOnePlusTwoThenThree() {
+        //Определяем ввод данных
+        double first = 1D;
+        double second = 2D;
+        // определяем ожидаемый результат (или поведение)
+        double expect = 3D;
+        //Составление списка действий и сценария. После выполния будет произведено какое-то действие, изменено состояние программы, либо выведено результат
+		Calculate calc = new Calculate();
+        calc.add(first, second);
+		double result = calc.getResult();
+        //завершающий этап. Сравнение реального и ожидаемого резальтатов программы
+        assertThat(result, is(expect));
+	}
+
+    /**
+     * Test subtract.
+     */
+    @Test
+    public void whenAddThreeSubtractTwoThenOne() {
+        //Определяем ввод данных
+        double first = 3D;
+        double second = 2D;
+        // определяем ожидаемый результат (или поведение)
+        double expect = 1D;
+        //Составление списка действий и сценария. После выполния будет произведено какое-то действие, изменено состояние программы, либо выведено результат
+        Calculate calc = new Calculate();
+        calc.subtruct(first, second);
+        double result = calc.getResult();
+        //завершающий этап. Сравнение реального и ожидаемого резальтатов программы
+        assertThat(result, is(expect));
     }
 
+    /**
+     * Test div.
+     */
+    @Test
+    public void whenAddFourDivTwoThenTwo() {
+        //Определяем ввод данных
+        double first = 4D;
+        double second = 2D;
+        // определяем ожидаемый результат (или поведение)
+        double expect = 2D;
+        //Составление списка действий и сценария. После выполния будет произведено какое-то действие, изменено состояние программы, либо выведено результат
+        Calculate calc = new Calculate();
+        calc.div(first, second);
+        double result = calc.getResult();
+        //завершающий этап. Сравнение реального и ожидаемого резальтатов программы
+        assertThat(result, is(expect));
+    }
 
     /**
      * Test add.
      */
-	/*public void when...Then... () {}*/
-	@Test
-    public void whenAddOneToOneThenTwo() {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		Calculate.main(null);
-		assertThat(
-			out.toString(),
-			is(
-				String.format(
-					"hello Job4j%s",
-					System.getProperty("line.separator")
-				)
-			)
-		);
+    @Test
+    public void whenAddTwoMultipleTreeThenSix() {
+        //Определяем ввод данных
+        double first = 2D;
+        double second = 3D;
+        // определяем ожидаемый результат (или поведение)
+        double expect = 6D;
+        //Составление списка действий и сценария. После выполния будет произведено какое-то действие, изменено состояние программы, либо выведено результат
+        Calculate calc = new Calculate();
+        calc.multiple(first, second);
+        double result = calc.getResult();
+        //завершающий этап. Сравнение реального и ожидаемого резальтатов программы
+        assertThat(result, is(expect));
     }
+
+
 }

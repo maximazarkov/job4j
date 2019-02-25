2a01b1615deda82495a091c114ce0c3f5225cf31
Maxim Azarkov <aza-maxim@yandex.ru>
2019-02-25 22:52:21 +0700

    4.4. Максимум из трех чисел[#113251]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -19,6 +19,17 @@ public class Max {
     }
 
     /**
+     * max - определяет максимальное число.
+     * @param first - первое число.
+     * @param second - второе число.
+     * @param third - третье число.
+     * @return - максимальное число.
+     */
+    public int max(int first, int second, int third) {
+        return this.max(this.max(first, second), third);
+    }
+
+    /**
      * summation - сумма двух чисел
      * @param first - первое число.
      * @param second - второе число.
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -20,6 +20,27 @@ public class MaxTest {
     }
 
     @Test
+    public void whenFirstGreatSecondAndThird() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 1, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenSecondGreatFirstAndThird() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenThirdGreatFirstAndSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 2, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
     public void whenSummation() {
         Max maxim = new Max();
         int result = maxim.summation(3, 2);

f5c9fadb87bf4697844843b138f6dd16bed7c009
Maxim Azarkov <aza-maxim@yandex.ru>
2019-04-13 00:41:22 +0700

    . Рефакторинг - Перенести общие методы в абстрактный класс[#113352] + BugFix

--- chapter_002/src/main/java/ru/job4j/tracker/ShowAllItems.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ShowAllItems.java
@@ -3,25 +3,11 @@ package ru.job4j.tracker;
 /**
  * Класс реализует вывод всеех заявок из хранилища.
  */
-public class ShowAllItems implements UserAction {
-    private int key;
-    private String info;
+public class ShowAllItems extends BaseAction {
 
     public ShowAllItems(int key, String info) {
-        this.key = key;
-        this.info = info;
+        super(key, info);
     }
-
-    @Override
-    public int key() {
-        return this.key;
-    }
-
-    @Override
-    public String info() {
-        return String.format("%s. %s", this.key(), this.info);
-    }
-
 
     @Override
     public void execute(Input input, Tracker tracker) {

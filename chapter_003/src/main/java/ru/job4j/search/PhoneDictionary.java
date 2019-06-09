950f51f85cc959f05f178d08644f5ad75acef2db
Maxim Azarkov <aza-maxim@yandex.ru>
2019-06-09 14:54:56 +0700

    1. Телефонный справочник на базе ArrayList[#113309]. Реализовал метод Find. написал тесты.

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,37 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.NoSuchElementException;
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+//        result.addAll(persons);
+        for (Person person : persons) {
+            if ((key.contains(person.getName()))
+                    || (key.equals(person.getSurname()))
+                    || (key.equals(person.getPhone()))
+                    || (key.equals(person.getAddress()))
+            ) {
+                result.add(person);
+            }
+
+        }
+//        if (result.size() == 0) {
+//            throw new NoSuchElementException("Совпадений нет. Повторите поиск с другими параметрами");
+//        }
+        return result;
+    }
+}

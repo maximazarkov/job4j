6bf3a333b6ce43d18c907457edacc210c06caf11
Maxim Azarkov <aza-maxim@yandex.ru>
2019-10-07 23:57:20 +0600

    0.1 Ссылки на методы.[#113209]-v0.1

--- chapter_004/src/main/java/ru/job4j/Calculator.java
+++ chapter_004/src/main/java/ru/job4j/Calculator.java
@@ -8,18 +8,10 @@ public class Calculator {
         double calc(int left, int right);
     }
 
-//    public void multiple(int start, int finish, int value, Operation op) {
-//        for (int index = start; index != finish; index++) {
-//            System.out.println(
-//                    op.calc(value, index)
-//            );
-//        }
-//    }
-
     public void multiple(int start, int finish, int value,
                          BiFunction<Integer, Integer, Double> op,
                          Consumer<Double> media) {
-        for(int index = start; index != finish; index++){
+        for (int index = start; index != finish; index++) {
             media.accept(op.apply(value, index));
         }
     }
@@ -28,18 +20,8 @@ public class Calculator {
         Calculator calc = new Calculator();
         calc.multiple(
                 0, 10, 2,
-                (left, right) -> {
-                    double result = left * right;
-                    System.out.printf("Multiple %s * %s = %s %n", left, right, result);
-                    return result;
-                },
-                result -> System.out.println(result)
-//                new Operation() {
-//                    @Override
-//                    public double calc(int left, int right) {
-//                        return left * right;
-//                    }
-//                }
+                MathUtil::add,
+                System.out::println
         );
     }
 }

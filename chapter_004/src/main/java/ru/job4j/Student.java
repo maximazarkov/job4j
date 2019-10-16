ea5e704fc90509023fae5761a84339574f02592b
Maxim Azarkov <aza-maxim@yandex.ru>
2019-10-17 00:08:43 +0700

    1. Фильтрация учеников.[#113214]-переопределены equals и hash-v0.3

--- chapter_004/src/main/java/ru/job4j/Student.java
+++ chapter_004/src/main/java/ru/job4j/Student.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import java.util.Objects;
+
 public class Student {
     private String nameStudent;
     private int score;
@@ -25,5 +27,35 @@ public class Student {
                 + "nameStudent='" + nameStudent + '\''
                 + ", score=" + score
                 + '}';
+    }
+
+    /**
+     * Переопределим методы equals(). Создадим условие, для работы метода с слассом Student
+     * Данный метод применяется в Тесте (в противном случае, даже при одинаковых классах - результатах) тесты падают
+     * @param o - объект для обработки (по сути Student)
+     * @return -  возвращаем результат сравнения объекта Student
+     * @since v0.3 17.10.2019
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return score == student.score
+                && Objects.equals(nameStudent, student.nameStudent);
+    }
+
+    /**
+     * переопределяется по умолчанию совместно с equals()
+     * @return - возвращает hash для класса Student
+     * @since v0.3 17.10.2019
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameStudent, score);
     }
 }
